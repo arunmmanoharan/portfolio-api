401f788230645f05a8b5d53203beafc0b104a447
Arun Manohar <a2441918@gmail.com>
2019-03-25 22:46:43 -0400

    Fixing cors

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const compression = require('compression');
 var app = express();
 app.use(compression());
 
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
 app.get('/', async (req, res) => {
 	try {
 		const resumeJson = await require('./resume.json');
