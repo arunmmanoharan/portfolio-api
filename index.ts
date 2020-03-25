3164743cd896ee01c66566acc6fe6f691e995c95
Arun Manohar Manoharan <amanoharan@trinitypartners.com>
2020-03-25 14:45:40 -0400

    Upgrade to typescript

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 const express = require('express');
 const compression = require('compression');
 
-var app = express();
+const app = express();
 app.use(compression());
 
 app.use((req, res, next) => {
